6b3c9621a7f7e0c065c93c862c46d5a391d3e125
morihirotoshida <97925123+morihirotoshida@users.noreply.github.com>
2022-01-18 13:52:04 +0900

    Clean up document

--- main01.js
+++ main01.js
@@ -1,3 +1,7 @@
+/* MIT License is recorded on January 18, 2022.
+ *Aushor: Morihiro Toshida
+ *Copyright 2021 Morihiro Toshida
+ */
 $(function(){
   $("a.dropdown-toggle").click(function(){
       $("div.menu").toggleClass('showUp');
